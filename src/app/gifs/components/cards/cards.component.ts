459111e13cc5454d404f76c745c7c97720c15dad
rubmanb <rubenm1386@gmail.com>
2024-05-10 11:51:56 +0200

    Adding card component

---
+++ src/app/gifs/components/cards/cards.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../interfaces/gifs.interface';
+
+@Component({
+  selector: 'gifs-cards-component',
+  templateUrl: './cards.component.html',
+  styleUrl: './cards.component.css'
+})
+export class CardsComponent implements OnInit{
+
+
+  @Input()
+  public gif!: Gif;
+
+  constructor(){}
+
+  ngOnInit(): void {
+    if(!this.gif) throw new Error('Gif not founded.');
+  }
+}
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
     HomePageComponent,
     SearchBoxComponent,
-    CardListComponent
+    CardListComponent,
+    CardsComponent
   ],
   imports: [
     CommonModule
